138a4410566d1b16202af6295eae18dcf0b47ee6
ksiks_wa3r <simpleteam2001@gmail.com>
2023-05-08 08:59:46 +0100

    add filter-navbar

--- parking-system-frontend/src/app/navbar-filters/navbar-filters.component.ts
+++ parking-system-frontend/src/app/navbar-filters/navbar-filters.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar-filters',
-  templateUrl: './navbar-filters.component.html',
-  styleUrls: ['./navbar-filters.component.css']
+	selector: 'app-navbar-filters',
+	templateUrl: './navbar-filters.component.html',
+	styleUrls: ['./navbar-filters.component.css']
 })
 export class NavbarFiltersComponent {
+	//cities
+	cities = Array(32).fill("Marrakech");
+	//mouse inside nav
+	mouseInside = false;
+	
+	onMouseEnterNavbar() {
+		this.mouseInside = true;
+	}
 
+	onMouseLeaveNavbar() {
+		this.mouseInside = false;
+	}
 }
