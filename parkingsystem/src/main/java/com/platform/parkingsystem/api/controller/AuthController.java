25f5dd091a3324f2731670e02ac423cf37cb8f9b
haaxm <38157591+haaxm@users.noreply.github.com>
2023-05-11 20:59:10 +0100

    few fixes (authentification)

---
+++ parkingsystem/src/main/java/com/platform/parkingsystem/api/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.platform.parkingsystem.api.controller;
+
+import com.platform.parkingsystem.api.exceptions.ResourceNotFoundException;
+import com.platform.parkingsystem.api.model.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+    @PostMapping("/login")
+    public CurrentUser login(@Valid @RequestBody LoginForm form, BindingResult bindingResult,
+                             HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            throw new ResourceNotFoundException("Invalid username or password");
+        }
+
+        try {
+            request.login(form.getUsername(), form.getPassword());
+        } catch (ServletException e) {
+            throw new ResourceNotFoundException("Invalid username or password");
+        }
+
+        var auth = (Authentication) request.getUserPrincipal();
+        var user = (User) auth.getPrincipal();
+
+
+        return new CurrentUser(user.getId(), user.getFirstName());
+    }
+
+    @PostMapping("/logout")
+    public LogoutResponse logout() {
+        return new LogoutResponse();
+    }
+
+    @GetMapping("/current-user")
+    public CurrentUser getCurrentUser(@AuthenticationPrincipal User user) {
+        return new CurrentUser(user.getId(), user.getFirstName());
+    }
+
+    public record CurrentUser(String id, String nickname) {}
+    public record LogoutResponse() {}
+}
