3ce9010957a9bed76cb0e35f4cd08f7325d6ad8a
haaxm <38157591+haaxm@users.noreply.github.com>
2023-05-25 23:05:55 +0100

    fixed small bugs related to Reservations , #9

---
+++ parkingsystem/src/main/java/com/platform/parkingsystem/api/controller/ReservationRequest.java
@@ -0,0 +1,11 @@
+package com.platform.parkingsystem.api.controller;
+
+import java.time.LocalDateTime;
+
+public class ReservationRequest {
+
+    public String parkinglotId;
+    public String from;
+
+    public String to;
+}
