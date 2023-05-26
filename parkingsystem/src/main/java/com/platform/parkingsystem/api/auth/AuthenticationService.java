9a54541b415acd4360b994820dd0beef75f0b5fa
haaxm <38157591+haaxm@users.noreply.github.com>
2023-05-26 13:20:47 +0100

    adaptations

--- parkingsystem/src/main/java/com/platform/parkingsystem/api/auth/AuthenticationService.java
+++ parkingsystem/src/main/java/com/platform/parkingsystem/api/auth/AuthenticationService.java
@@ -66,6 +66,7 @@ public class AuthenticationService {
     return AuthenticationResponse.builder()
         .accessToken(jwtToken)
             .refreshToken(refreshToken)
+            .user_id(user.getId())
         .build();
   }
 
